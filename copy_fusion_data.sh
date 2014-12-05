d3b0c0f1a9dce1c959cc7d7e1c674e0f314f9f99
Rebecca Roelofs <roelofs@cs.berkeley.edu>
2014-12-05 09:40:32 +0000

    Fix script

--- copy_fusion_data.sh
+++ copy_fusion_data.sh
@@ -15,6 +15,6 @@ do
   /root/ephemeral-hdfs/bin/hadoop distcp s3n://imagenet-linear-solver-data/daisy-aPart$i-$i /daisy-aPart$i-$i
   /root/ephemeral-hdfs/bin/hadoop distcp s3n://imagenet-linear-solver-data/daisy-testFeatures-test-$i /daisy-testFeatures-test-$i
 
-  /root/ephemeral-hdfs/bin/hadoop distcp s3n://imagenet-linear-solver-data/lcs-aPart$i-$i /lcs-aPart1-$i
+  /root/ephemeral-hdfs/bin/hadoop distcp s3n://imagenet-linear-solver-data/lcs-aPart$i-$i /lcs-aPart$i-$i
   /root/ephemeral-hdfs/bin/hadoop distcp s3n://imagenet-linear-solver-data/lcs-testFeatures-test-$i /lcs-testFeatures-test-$i
 done
