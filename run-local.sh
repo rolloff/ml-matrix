d05aa8fdc571fd559f8deed7766ab679b5c4482f
Rebecca Roelofs <roelofs@berkeley.edu>
2015-03-05 17:54:10 -0800

    script to run local experiments

---
+++ run-local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SPARK_MEM=8g
+SPARK_MASTER="local"
+DATA_DIR="imagenet-linear-solver-data/"
+PARTS=8
+LAMBDA=0.1
+CLASS="CheckQR"
+THRESH=1e-8
+DATASET="daisy"
+
+export SPARK_MEM
+
+ID=$CLASS-$DATASET-$PARTS-$LAMBDA-$THRESH-`date +"%Y_%m_%d_%H_%M_%S"`
+
+./run-main.sh edu.berkeley.cs.amplab.mlmatrix.$CLASS $SPARK_MASTER $DATA_DIR $PARTS $LAMBDA $THRESH $DATASET 2>$ID.stderr 1>$ID.stdout
